622dc2fb6f878a88f99adfa610cc62b2c43b0065
safasf2fds <gs1melihaya@hotmail.com>
2024-05-06 13:09:35 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-function isPalindrome(str) {
-  const reversed = str.split("").reverse().join("");
-  return str === reversed;
-}
-
-console.log(isPalindrome("racecar")); // true
+const stoogeSort = (arr, i = 0, j = arr.length - 1) => {
+  if (arr[i] > arr[j]) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  if (i + 1 >= j) {
+    return arr;
+  }
+  const t = Math.floor((j - i + 1) / 3);
+  stoogeSort(arr, i, j - t);
+  stoogeSort(arr, i + t, j);
+  stoogeSort(arr, i, j - t);
+  return arr;
+};
