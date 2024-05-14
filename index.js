aac383750eb19269e36752c4a9a11a41bfc9054e
safasf2fds <gs1melihaya@hotmail.com>
2024-05-14 21:17:33 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
+function permute(nums) {
+  const result = [];
+  backtrack([]);
+  return result;
+  function backtrack(current) {
+    if (current.length === nums.length) {
+      result.push([...current]);
+      return;
+    }
+    for (const num of nums) {
+      if (current.includes(num)) continue;
+      current.push(num);
+      backtrack(current);
+      current.pop();
+    }
+  }
+}
